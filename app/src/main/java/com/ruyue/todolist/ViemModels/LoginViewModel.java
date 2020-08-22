8eaa2fca8fc5e79c08b06958bc92b29dd36c9dff
lpdft <772346730@qq.com>
2020-08-22 23:50:36 +0800

    [ruyue] feat-#1:add MVVM files

---
+++ app/src/main/java/com/ruyue/todolist/ViemModels/LoginViewModel.java
@@ -0,0 +1,12 @@
+package com.ruyue.todolist.ViemModels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class LoginViewModel extends AndroidViewModel {
+    public LoginViewModel(@NonNull Application mContext) {
+        super(mContext);
+    }
+}
