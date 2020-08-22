fc1cc8c809ccd72ef6582475edb587f3bc96de32
lpdft <772346730@qq.com>
2020-08-22 17:21:57 +0800

    [ruyue] feat-#1:set up MVVM framework

--- app/src/main/java/com/ruyue/todolist/View/LoginActivity.java
+++ app/src/main/java/com/ruyue/todolist/View/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.ruyue.todolist;
+package com.ruyue.todolist.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ruyue.todolist.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
