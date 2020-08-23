ca8bf4b652b2b96d1ecb405c377dbf9e87e53c5a
lpdft <772346730@qq.com>
2020-08-23 13:25:48 +0800

    [ruyue] feat-#1:click login button to hit error message when name and password error

--- app/src/main/java/com/ruyue/todolist/models/User.java
+++ app/src/main/java/com/ruyue/todolist/models/User.java
@@ -9,6 +9,12 @@ public class User {
     public User() {
     }
 
+    public User(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
     public User(String name, String password) {
         this.name = name;
         this.password = password;
--- app/src/main/java/com/ruyue/todolist/view/LoginActivity.java
+++ app/src/main/java/com/ruyue/todolist/view/LoginActivity.java
@@ -3,38 +3,59 @@ package com.ruyue.todolist.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
-import android.text.Html;
+
 import android.text.TextWatcher;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.ruyue.todolist.BR;
+import com.google.gson.Gson;
 import com.ruyue.todolist.models.User;
 import com.ruyue.todolist.R;
 import com.ruyue.todolist.databinding.ActivityLoginBinding;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.util.Objects;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class LoginActivity extends AppCompatActivity {
+    private static final String URL = "https://twc-android-bootcamp.github.io/fake-data/data/user.json";
     private Button loginBtn;
     private Boolean inputNameLegal = false;
     private Boolean inputPasswordLegal = false;
+    private EditText editTextName;
+    private User ServiceUser;
+    private EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         final ActivityLoginBinding activityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        editTextName = findViewById(R.id.name);
+        editTextPassword = findViewById(R.id.password);
         loginBtn = findViewById(R.id.login);
+
+        loginBtn.setOnClickListener(v -> {
+            String name = editTextName.getText().toString();
+            String password = editTextPassword.getText().toString();
+            getUserInfo(name, password);
+        });
+
         judgeNameIsLegal();
         judgePasswordIsLegal();
     }
 
     private void judgeNameIsLegal() {
-        final EditText editTextName = findViewById(R.id.name);
         TextWatcher watcher = new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
@@ -51,7 +72,7 @@ public class LoginActivity extends AppCompatActivity {
                     } else {
                         inputNameLegal = true;
                         if(inputPasswordLegal) {
-                            loginBtn.setEnabled(false);
+                            loginBtn.setEnabled(true);
                             loginBtn.setBackground(getResources().getDrawable(R.drawable.login_btn_enable, null));
                         }
                     }
@@ -62,7 +83,6 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void judgePasswordIsLegal() {
-        final EditText editTextPassword = findViewById(R.id.password);
         TextWatcher watcherPassword = new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
@@ -87,5 +107,47 @@ public class LoginActivity extends AppCompatActivity {
             }
         };
         editTextPassword.addTextChangedListener(watcherPassword);
+    }
+
+    private void getUserInfo(String name, String password) {
+        final Request request = new Request.Builder().url(URL).build();
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Call call = okHttpClient.newCall(request);
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull final IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                final String result = Objects.requireNonNull(response.body()).string();
+                jsonStringToUserList(result);
+                runOnUiThread(() -> {
+                    judgeUser(name, password);
+                });
+            }
+        });
+    }
+
+    public void jsonStringToUserList(String result) {
+        Gson gson = new Gson();
+        ServiceUser = gson.fromJson(result, User.class);
+    }
+
+    public void judgeUser(String name, String password) {
+        if(name.equals(ServiceUser.getName())) {
+            if(!password.equals(ServiceUser.getPassword())) {
+                Toast.makeText(getApplicationContext(), "密码错误", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "用户不存在", Toast.LENGTH_SHORT).show();
+        }
     }
 }
