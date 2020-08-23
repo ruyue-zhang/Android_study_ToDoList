9ed97c2c885e80a7e2994166ebc9e2d0d3a3cbc8
lpdft <772346730@qq.com>
2020-08-23 10:59:03 +0800

    [ruyue] feat-#1:disable login button when input error

--- app/src/main/java/com/ruyue/todolist/view/LoginActivity.java
+++ app/src/main/java/com/ruyue/todolist/view/LoginActivity.java
@@ -9,6 +9,7 @@ import android.text.Editable;
 import android.text.Html;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,12 +19,16 @@ import com.ruyue.todolist.R;
 import com.ruyue.todolist.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button loginBtn;
+    private Boolean inputNameLegal = false;
+    private Boolean inputPasswordLegal = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         final ActivityLoginBinding activityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        loginBtn = findViewById(R.id.login);
         judgeNameIsLegal();
         judgePasswordIsLegal();
     }
@@ -37,8 +42,19 @@ public class LoginActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) { }
             @Override
             public void afterTextChanged(Editable s) {
-                if (editTextName.getText().length() > 0 && !editTextName.getText().toString().matches("^[a-zA-Z0-9]{3,12}$")) {
-                    editTextName.setError("用户名必须是3 ~ 12为字母或数字");
+                if (editTextName.getText().length() > 0) {
+                    if(!editTextName.getText().toString().matches("^[a-zA-Z0-9]{3,12}$")) {
+                        editTextName.setError("用户名必须是3 ~ 12为字母或数字");
+                        loginBtn.setEnabled(false);
+                        inputNameLegal = false;
+                        loginBtn.setBackground(getResources().getDrawable(R.drawable.login_btn_layout, null));
+                    } else {
+                        inputNameLegal = true;
+                        if(inputPasswordLegal) {
+                            loginBtn.setEnabled(false);
+                            loginBtn.setBackground(getResources().getDrawable(R.drawable.login_btn_enable, null));
+                        }
+                    }
                 }
             }
         };
@@ -46,7 +62,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void judgePasswordIsLegal() {
-        final EditText editTextPassword = findViewById(R.id.user_password);
+        final EditText editTextPassword = findViewById(R.id.password);
         TextWatcher watcherPassword = new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
@@ -54,8 +70,19 @@ public class LoginActivity extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) { }
             @Override
             public void afterTextChanged(Editable s) {
-                if (editTextPassword.getText().length() > 0 && !editTextPassword.getText().toString().matches("^.{6,18}$")) {
-                    editTextPassword.setError("密码长度必须是6 ~ 18位字符");
+                if (editTextPassword.getText().length() > 0) {
+                    if (!editTextPassword.getText().toString().matches("^.{6,18}$")) {
+                        editTextPassword.setError("密码长度必须是6 ~ 18位字符");
+                        loginBtn.setEnabled(false);
+                        inputPasswordLegal = false;
+                        loginBtn.setBackground(getResources().getDrawable(R.drawable.login_btn_layout, null));
+                    } else {
+                        inputPasswordLegal = true;
+                        if(inputNameLegal) {
+                            loginBtn.setEnabled(true);
+                            loginBtn.setBackground(getResources().getDrawable(R.drawable.login_btn_enable, null));
+                        }
+                    }
                 }
             }
         };
