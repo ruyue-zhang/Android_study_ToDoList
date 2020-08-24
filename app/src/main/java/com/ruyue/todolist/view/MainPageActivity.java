6613addc3623763b632934dba57199f8eae2385b
lpdft <772346730@qq.com>
2020-08-24 22:20:21 +0800

    [ruyue] feat-#3:click create button jump to createTask page

--- app/src/main/java/com/ruyue/todolist/view/MainPageActivity.java
+++ app/src/main/java/com/ruyue/todolist/view/MainPageActivity.java
@@ -2,7 +2,9 @@ package com.ruyue.todolist.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ruyue.todolist.R;
 
@@ -12,6 +14,15 @@ public class MainPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        findViewById(R.id.jump_to_create).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainPageActivity.this, CreateTaskActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
