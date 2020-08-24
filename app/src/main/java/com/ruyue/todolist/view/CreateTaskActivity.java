e4ff90a9a5b845d503d7afb5ba40f021e0bf694e
lpdft <772346730@qq.com>
2020-08-25 00:19:04 +0800

    [ruyue] feat-#3:add layout view

--- app/src/main/java/com/ruyue/todolist/view/CreateTaskActivity.java
+++ app/src/main/java/com/ruyue/todolist/view/CreateTaskActivity.java
@@ -3,8 +3,14 @@ package com.ruyue.todolist.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
 
 import com.ruyue.todolist.R;
+
+import java.util.GregorianCalendar;
 
 public class CreateTaskActivity extends AppCompatActivity {
 
@@ -12,5 +18,13 @@ public class CreateTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+
+        Button dateBtn = findViewById(R.id.date);
+        dateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
