bcef49b6e68ff0c149dc15c9e4e05306f80734ae
lpdft <772346730@qq.com>
2020-08-25 12:04:44 +0800

    [ruyue] fixed-#3:change ActionBar back_btn color

--- app/src/main/java/com/ruyue/todolist/view/CreateTaskActivity.java
+++ app/src/main/java/com/ruyue/todolist/view/CreateTaskActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -29,6 +32,10 @@ public class CreateTaskActivity extends AppCompatActivity {
 
         binding.setLifecycleOwner(this);
         binding.setCreateTaskViewModel(createTaskViewModel);
+        getSupportActionBar().setElevation(0); //去掉ActionBar的分割线
+        Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_material);
+        upArrow.setColorFilter(getResources().getColor(R.color.btn_text_color), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
 
         //如果创建成功（create_success按钮被点击），jumpToMainPageWithData函数
         findViewById(R.id.create_success).setOnClickListener(new View.OnClickListener() {
