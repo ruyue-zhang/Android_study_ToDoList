7264318d550a5644292ec91c3358a65f375b2885
lpdft <772346730@qq.com>
2020-08-26 17:23:41 +0800

    [ruyue] fixed-#2:change Button to FloatButton

--- app/src/main/java/com/ruyue/todolist/view/MainPageActivity.java
+++ app/src/main/java/com/ruyue/todolist/view/MainPageActivity.java
@@ -5,8 +5,14 @@ import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -42,22 +48,17 @@ public class MainPageActivity extends AppCompatActivity {
         binding.setMainPageViewModel(mainPageViewModel);
         getSupportActionBar().setElevation(0);
 
-        while(taskList == null) {
+        while (taskList == null) {
             taskList = mainPageViewModel.getTaskList();
         }
-
         Collections.sort(taskList);
-//
-//        taskList.stream().sorted(Comparator.comparing(simpleDateFormat.parse(task::getDate)));
         ListView listViewTask = findViewById(R.id.task_list_view);
         taskAdapter = new TaskAdapter(MainPageActivity.this, taskList);
         listViewTask.setAdapter(taskAdapter);
 
-        findViewById(R.id.jump_to_create).setOnClickListener(new View.OnClickListener()
-        {
+        findViewById(R.id.jump_to_create).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 Intent intent = new Intent(MainPageActivity.this, CreateTaskActivity.class);
                 startActivity(intent);
             }
@@ -72,5 +73,15 @@ public class MainPageActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         moveTaskToBack(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        MenuItem item = menu.findItem(R.id.exit);
+        SpannableString spannableString = new SpannableString(item.getTitle());
+        spannableString.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.btn_text_color)),0,spannableString.length(),0);
+        item.setTitle(spannableString);
+        return super.onCreateOptionsMenu(menu);
     }
 }
